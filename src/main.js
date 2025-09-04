5df31abb33a6f83f04eac928b03a62bf2812f632
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-09-03 23:01:57 -0300

    style: reorganizando codigo

--- src/main.js
+++ src/main.js
@@ -1,22 +1,30 @@
 export function setupCounter(element) {
   let counter = 0;
 
-  const adjustCounterValue = value => {
+  const adjustCounterValue = (value) => {
     if (value >= 100) return value - 100;
     if (value <= -100) return value + 100;
     return value;
   };
 
-  const setCounter = value => {
+  const setCounter = (value) => {
     counter = adjustCounterValue(value);
     const text = `${counter}`;
     element.innerHTML = text;
   };
 
-  document.getElementById('increaseByOne').addEventListener('click', () => setCounter(counter + 1));
-  document.getElementById('decreaseByOne').addEventListener('click', () => setCounter(counter - 1));
-  document.getElementById('increaseByTwo').addEventListener('click', () => setCounter(counter + 2));
-  document.getElementById('decreaseByTwo').addEventListener('click', () => setCounter(counter - 2))
+  document
+    .getElementById('increaseByOne')
+    .addEventListener('click', () => setCounter(counter + 1));
+  document
+    .getElementById('decreaseByOne')
+    .addEventListener('click', () => setCounter(counter - 1));
+  document
+    .getElementById('increaseByTwo')
+    .addEventListener('click', () => setCounter(counter + 2));
+  document
+    .getElementById('decreaseByTwo')
+    .addEventListener('click', () => setCounter(counter - 2));
 
   setCounter(0);
 }
